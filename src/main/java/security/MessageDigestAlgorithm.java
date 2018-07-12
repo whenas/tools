43db7d7b40abf673d266825eaf56d3d55007d5ed
lusongjiong <497236472@qq.com>
2018-07-12 23:38:35 +0800

    增加hmac和sha

--- src/main/java/security/MessageDigestAlgorithm.java
+++ src/main/java/security/MessageDigestAlgorithm.java
@@ -1,7 +1,12 @@
 package security;
 
+import org.apache.commons.codec.binary.Hex;
 import sun.misc.BASE64Encoder;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -17,11 +22,12 @@ import java.security.NoSuchAlgorithmException;
 public class MessageDigestAlgorithm {
 
     private static String message = "hello word";
+
     /**
      * MD5（128位摘要信息）+BASE64(BASE64 严格地说，属于编码格式，而非加密算法)
      * apache DigestUtils也可实现
      */
-    public String encryptMD5(String message){
+    public String encryptMD5(String message) {
         try {
             MessageDigest md = MessageDigest.getInstance("Md5");
             byte[] md5Bytes = md.digest(message.getBytes());
@@ -37,9 +43,40 @@ public class MessageDigestAlgorithm {
      * 安全散列算法
      * 固定长度摘要信息
      * SHA-1、SHA-2（SHA-224\SHA-256\SHA384\SHA-512）
-     * @param args
      */
+    public String encodeSHA256(String message) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] sha = md.digest(message.getBytes());
+            return new BASE64Encoder().encode(sha);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    /**
+     * HMAC 含有密钥的散列函数算法
+     * 融合MD、SHA
+     * MD系列：HmacMD2\HmacMD4\HmacMD5
+     * SHA系列:HmacSHA1\HmacSHA224\HmacSHA256\HmacSHA384\HmacSHA512
+     * @param
+     */
+    public String hmacMD5(String message){
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacMD5");//初始化keyGenerator
+            SecretKey secretKey = keyGenerator.generateKey();//产生密钥
+            byte[] key = secretKey.getEncoded();//获得密钥
+            SecretKey restoreSecretKey = new SecretKeySpec(key,"HmacMD5");
+            Mac mac = Mac.getInstance("HmacMD5");
+            mac.init(restoreSecretKey);//初始化mac
+            byte[] resultBytes = mac.doFinal(message.getBytes());
+            return Hex.encodeHexString(resultBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 
@@ -47,6 +84,9 @@ public class MessageDigestAlgorithm {
         MessageDigestAlgorithm mda = new MessageDigestAlgorithm();
         String str = mda.encryptMD5(message);
         System.out.println(str);
+        System.out.println(mda.encodeSHA256(message));
+        System.out.println(mda.hmacMD5(message));
+
 
     }
 
